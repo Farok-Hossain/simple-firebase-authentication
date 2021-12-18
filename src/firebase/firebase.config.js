cb644006cd146f9b5da5c41b29b87c4e1746bba4
Farok-Hossain <farokafs7@gmail.comm>
2021-12-19 00:49:39 +0600

    file added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAomumkQHvEHcEnqJxs5pIsvmXp5dfef2g",
+    authDomain: "simple-firebase-authenti-bc47d.firebaseapp.com",
+    projectId: "simple-firebase-authenti-bc47d",
+    storageBucket: "simple-firebase-authenti-bc47d.appspot.com",
+    messagingSenderId: "121499357938",
+    appId: "1:121499357938:web:3c9ae365034483f378366b"
+  };
+
+  export default firebaseConfig;
